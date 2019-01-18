a609e14e6570608872c308c070a41f927d3fd7f7
Ndelforno <nicolas.delforno@gmail.com>
2019-01-18 10:49:05 -0500

    Authentication controller defined + route

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe AuthenticationController, type: :controller do
+  # return auth token once user is authenticated
+  def authenticate
+    auth_token =
+      AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+    json_response(auth_token: auth_token)
+  end
+
+  private
+
+  def auth_params
+    params.permit(:email, :password)
+  end
 
 end
